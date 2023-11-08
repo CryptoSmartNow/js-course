0d4b7ca5ab053fea2b06b8a891daa75de88c5948
Great Adams <kingifean@gmail.com>
2023-11-08 10:56:33 +0100

    strings and vars

---
+++ string.js
@@ -0,0 +1,17 @@
+
+const firstName = 'Great'
+let middleName = "London"
+var lastName = `Adams`
+
+// const fullName = firstName + ' ' + middleName + ' ' + lastName
+
+let fullName = `${firstName} ${middleName} ${lastName}`
+
+const text = `Hello everyone sorry the teacher won't be arround he said know "Please finish copying your notes"
+
+We stopped at page 5
+
+`
+
+console.log(fullName)
+// let greatAdamsIfeayiChukwuLondon
---
+++ vars.js
@@ -0,0 +1,15 @@
+// let x = 'Adams'
+
+// let x = 'Great'
+// console.log(x)
+
+// const is the keyword that tells JavaScript you’re declaring a variable.
+// x is the name of that variable.
+// = is the operator that tells JavaScript a value is coming up next.
+// 'Adams' is the value for the variable to store.
+
+// Types of variable declarations
+// var, let, const
+
+x = 4
+console.log(x)
